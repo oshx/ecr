cb569290736fdd8ab7e7140be59742a492fbcd15
오승환 <oshxios@gmail.com>
2024-04-09 16:07:40 +0900

    공유하기와 다시하기, 제작 정보 추가

---
+++ src/components/Credit.svelte
@@ -0,0 +1,4 @@
+<footer>
+  <cite>&copy; OSHx</cite>
+  <address>예깅</address>
+</footer>
--- src/pages/Result.page.svelte
+++ src/pages/Result.page.svelte
@@ -1,34 +1,44 @@
 <script>
   import { beforeUpdate } from "svelte";
   import { convertParamToScore, convertScoreToResult } from "../core/ecr";
+  import { SERVICE_NAME } from "../core/constants";
   import ExperimentSheet from "../components/ExperimentSheet.svelte";
+  import { PATH_EXPERIMENT } from "../router/routes.config";
+  import { href, link } from "../router/helper";
 
   export let params;
 
   let error = null;
   let scoreList = [];
   let result = { title: null, content: null };
+  let title = SERVICE_NAME;
 
   async function generateResult() {
     try {
       scoreList = convertParamToScore(params.result);
       result = convertScoreToResult(scoreList);
+      title = `${result.title} - ${SERVICE_NAME}`;
       error = null;
     } catch (parsingError) {
       error = parsingError;
     }
   }
 
-  function handleRetry() {
+  function handleShare() {
     if (window.navigator.share) {
       window.navigator.share({
-
-      })
+        title: `${result.title} - ${SERVICE_NAME}`,
+        url: window.location.href,
+      });
     }
   }
 
   beforeUpdate(generateResult);
 </script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
 
 {#if error === null && result.title && result.content}
   <main class="result">
@@ -36,12 +46,12 @@
     <p>{result.content}</p>
   </main>
   <div class="action">
-    <button class="" on:click={handleRetry}>다시 검사하기</button>
+    <a class="action__trigger action__trigger--retry" href={href(PATH_EXPERIMENT)} use:link>다시 검사하기</a>
+    <button class="action__trigger action__trigger--share" on:click={handleShare}>공유하기</button>
   </div>
   <div class="answer">
     <h2>응답</h2>
-    <ExperimentSheet answerList={scoreList} readonly={true} vertical={true}
-    />
+    <ExperimentSheet answerList={scoreList} readonly={true} vertical={true} />
   </div>
 {:else}
   {error?.message}
@@ -59,7 +69,7 @@
     font-size: 24px;
     font-weight: 700;
     letter-spacing: -1px;
-    border-bottom: 1px solid rgba(0, 0, 0, .12);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.12);
     color: #666;
   }
 
@@ -67,7 +77,24 @@
     font-size: 18px;
     line-height: 1.8;
     letter-spacing: -1px;
-    text-indent: .5em;
+    text-indent: 0.5em;
     color: #333;
+  }
+
+  .action {
+    overflow: hidden;
+  }
+  .action__trigger {
+    text-decoration: none;
+    font-style: normal;
+    background: none transparent;
+    border: 0 none;
+    cursor: pointer;
+  }
+  .action__trigger--retry {
+    color: #f00;
+  }
+  .action__trigger--share {
+    color: #369;
   }
 </style>
